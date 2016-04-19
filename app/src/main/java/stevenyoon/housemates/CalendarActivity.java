19402c92e5edf4ea72c0b9d78c978c7edfcb349c
Mikael <mantism@seas.upenn.edu>
2016-04-18 22:33:54 -0400

    fixed calendar activity

--- app/src/main/java/stevenyoon/housemates/CalendarActivity.java
+++ app/src/main/java/stevenyoon/housemates/CalendarActivity.java
@@ -1,54 +1,41 @@
 package stevenyoon.housemates;
-/**
- * Created by Mikael Mantis 3/16/16
- */
-import java.util.Calendar;
+
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import stevenyoon.housemates.Event;
-import java.util.ArrayList;
-import android.app.AlertDialog.Builder;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.content.DialogInterface;
-import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.view.LayoutInflater;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import android.widget.ListView;
-
-import android.view.ViewGroup;
-import android.view.InflateException;
-import android.widget.Button;
-
-import android.app.DatePickerDialog;
-import android.app.DatePickerDialog.OnDateSetListener;
-import android.widget.DatePicker;
-import android.app.TimePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.text.format.DateFormat;
-import android.widget.TimePicker;
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     private List<Event> list;
     private ListView listItems;
     private EventAdapter adapt;
@@ -61,20 +48,21 @@ public class CalendarActivity extends AppCompatActivity
     String eventTimeE;
     String eventClub;
     String eventDetails;
-
+    Date startTime;
+    Date endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-       /* DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
-        toggle.syncState();*/
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -84,7 +72,6 @@ public class CalendarActivity extends AppCompatActivity
         adapt = new EventAdapter(this, R.layout.event_list_inner_view, list);
         listItems.setAdapter(adapt);
         setupListViewListener();
-
     }
 
     public void addEvent(View v) {
@@ -104,22 +91,22 @@ public class CalendarActivity extends AppCompatActivity
                 parent.removeView(textEventEntry);
             }
         }
-       /* try {
-            textEventEntry = factory.inflate(R.layout.event_prompt, null, false);
-        } catch (InflateException e) {
 
-        }*/
         final Calendar myCalendar = Calendar.getInstance();
 
         final EditText eventNameInput = (EditText) textEventEntry.findViewById(R.id.newEventName);
         final EditText eventDateInput = (EditText) textEventEntry.findViewById(R.id.newEventDate);
+        eventDateInput.setFocusable(false);
         final EditText eventTimeStartInput = (EditText) textEventEntry.findViewById(R.id
                 .newEventTimeS);
+        eventTimeStartInput.setFocusable(false);
         final EditText eventTimeEndInput = (EditText) textEventEntry.findViewById(R.id
                 .newEventTimeE);
+        eventTimeEndInput.setFocusable(false);
         final EditText eventClubInput = (EditText) textEventEntry.findViewById(R.id.newEventClub);
         final EditText eventDetailsInput = (EditText) textEventEntry.findViewById(R.id
                 .newEventDetails);
+        final Date currentDate = myCalendar.getTime();
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
@@ -132,8 +119,14 @@ public class CalendarActivity extends AppCompatActivity
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 String myFormat = "MM/dd/yy";
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                Date d = myCalendar.getTime();
+                if (d.before(currentDate)) {
+                    System.out.println("test");
+                    eventDateInput.setText("Pick a date after today");
+                } else {
+                    eventDateInput.setText(sdf.format(myCalendar.getTime()));
+                }
 
-                eventDateInput.setText(sdf.format(myCalendar.getTime()));
             }
 
         };
@@ -155,6 +148,7 @@ public class CalendarActivity extends AppCompatActivity
                 myCalendar.set(Calendar.MINUTE, minute);
                 String myFormat = "h:mm a";
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                startTime = myCalendar.getTime();
 
                 eventTimeStartInput.setText(sdf.format(myCalendar.getTime()));
             }
@@ -177,7 +171,7 @@ public class CalendarActivity extends AppCompatActivity
                 myCalendar.set(Calendar.MINUTE, minute);
                 String myFormat = "h:mm a";
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
+                endTime = myCalendar.getTime();
                 eventTimeEndInput.setText(sdf.format(myCalendar.getTime()));
             }
         };
@@ -224,49 +218,7 @@ public class CalendarActivity extends AppCompatActivity
         prompt.show();
 
 
-
     }
-
-  /*  public static class TimePickerFragment extends DialogFragment implements TimePickerDialog
-            .OnTimeSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
-
-        }
-    }*/
-
-    /*public static class DatePickerFragment extends DialogFragment implements DatePickerDialog
-            .OnDateSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onDateSet(Datepicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
-        }
-    }*/
 
     @Override
     public void onBackPressed() {
@@ -281,7 +233,7 @@ public class CalendarActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.calendar_activity, menu);
         return true;
     }
 
@@ -300,28 +252,26 @@ public class CalendarActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_calendar) {
-            // Handle the camera action
-        }
-        else if (id == R.id.nav_tasks) {
+        if (id == R.id.nav_home) {
+            Intent i = new Intent(CalendarActivity.this, MainActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_calendar) {
+            Intent i = new Intent(CalendarActivity.this, CalendarActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_tasks) {
             Intent i = new Intent(CalendarActivity.this, TasksActivity.class);
             startActivity(i);
-        }
-        else if (id == R.id.nav_payment) {
+        } else if (id == R.id.nav_payment) {
 
-        }
-        else if (id == R.id.nav_settings) {
-            Intent i = new Intent(CalendarActivity.this, SettingsActivity.class);
-            startActivity(i);
-        }
-        else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        }
-        else if (id == R.id.nav_messaging) {
+        } else if (id == R.id.nav_send) {
 
         }
 
@@ -352,14 +302,14 @@ public class CalendarActivity extends AppCompatActivity
         List<Event> eventList = new ArrayList<Event>();
         int layoutResourceId;
         public EventAdapter(Context context, int layoutResourceId,
-                         List<Event> objects) {
+                            List<Event> objects) {
             super(context, layoutResourceId, objects);
             this.layoutResourceId = layoutResourceId;
             this.eventList = objects;
             this.context = context;
         }
         /**
-         * This method will DEFINe what the view inside the list view will
+         * This method will Define what the view inside the list view will
          * finally look like Here we are going to code that the checkbox state
          * is the status of task and check box text is the task name
          */
