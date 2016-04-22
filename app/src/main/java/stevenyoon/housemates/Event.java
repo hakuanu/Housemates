96e5922e986e8a6043ba91e58052da158f05983d
Kevin Ho <kevinjho1996@gmail.com>
2016-04-22 00:10:44 -0400

    events sync with db

--- app/src/main/java/stevenyoon/housemates/Event.java
+++ app/src/main/java/stevenyoon/housemates/Event.java
@@ -1,6 +1,4 @@
 package stevenyoon.housemates;
-import java.util.Date;
-import java.sql.Time;
 /**
  * Created by Mikael on 3/18/16.
  */
@@ -12,18 +10,17 @@ public class Event {
     private String eventName;
     private String clubName;
     private String details;
-    private int id;
+    private String id;
     public static int max_event_id = 0;
 
-    public Event (String d, String s, String e, String eventName, String clubName, String details) {
+    public Event (String d, String s, String e, String eventName, String clubName, String details, String id) {
         this.date = d;
         this.startT = s;
         this.endT = e;
         this.eventName = eventName;
         this.clubName = clubName;
         this.details = details;
-        this.id = max_event_id;
-        max_event_id ++;
+        this.id = id;
 
     }
 
@@ -33,7 +30,7 @@ public class Event {
     public String getEndTime() {return endT;}
     public String getClubName() {return clubName;}
     public String getDetails() {return details;}
-    public int getId() {return id;}
+    public String getId() {return id;}
 
     public void updateEventName(String e) {this.eventName = e;}
     public void updateDate(String d) {this.date = d;}
